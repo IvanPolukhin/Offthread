8a34632083c84ca57d9c02f4244211e0dc1c837a
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2025-04-25 20:26:56 +0300

    refactor: move logic to custom hook

---
+++ src/components/PostList/usePostList.ts
@@ -0,0 +1,13 @@
+import { useStore } from 'src/store';
+import { useEffect } from 'react';
+
+export const usePostList = () => {
+  const { posts, isLoading, error } = useStore();
+  const fetchNextPage = useStore((s) => s.fetchNextPage);
+
+  useEffect(() => {
+    fetchNextPage();
+  }, [fetchNextPage]);
+
+  return { posts, isLoading, error, fetchNextPage };
+}
